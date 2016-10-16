6b2df5f5026dc74caf313949af9b764730343769
VivianLow <vivian198912@gmail.com>
2016-10-16 23:38:35 +0800

    Provide #36

--- src/test/java/seedu/task/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/task/testutil/TypicalTestTasks.java
@@ -10,23 +10,21 @@ import seedu.task.model.task.UniqueTaskList.DuplicateTaskException;
  */
 public class TypicalTestTasks {
 
-    public static TestTask assignment, benson, carl, daniel, elle, fiona, george, hoon, ida;
+    public static TestTask assignment, meeting, test, exam, project, movie, discussion, report, powerpoint;
 
     public TypicalTestTasks() {
         try {
             assignment =  new TaskBuilder().withName("assignment").build();
-//            benson = new TaskBuilder().withName("Benson Meier").withAddress("311, Clementi Ave 2, #02-25")
-//                    .withEmail("johnd@gmail.com").withPhone("98765432")
-//                    .withTags("owesMoney", "friends").build();
-//            carl = new TaskBuilder().withName("Carl Kurz").withPhone("95352563").withEmail("heinz@yahoo.com").withAddress("wall street").build();
-//            daniel = new TaskBuilder().withName("Daniel Meier").withPhone("87652533").withEmail("cornelia@google.com").withAddress("10th street").build();
-//            elle = new TaskBuilder().withName("Elle Meyer").withPhone("9482224").withEmail("werner@gmail.com").withAddress("michegan ave").build();
-//            fiona = new TaskBuilder().withName("Fiona Kunz").withPhone("9482427").withEmail("lydia@gmail.com").withAddress("little tokyo").build();
-//            george = new TaskBuilder().withName("George Best").withPhone("9482442").withEmail("anna@google.com").withAddress("4th street").build();
-//
-//            //Manually added
-//            hoon = new TaskBuilder().withName("Hoon Meier").withPhone("8482424").withEmail("stefan@mail.com").withAddress("little india").build();
-//            ida = new TaskBuilder().withName("Ida Mueller").withPhone("8482131").withEmail("hans@google.com").withAddress("chicago ave").build();
+            meeting = new TaskBuilder().withName("meeting with John").build();
+            test = new TaskBuilder().withName("driving test").build();
+            exam = new TaskBuilder().withName("programming exam").build();
+            project = new TaskBuilder().withName("software engineering project assignment").build();
+            movie = new TaskBuilder().withName("watch The Accountant").build();
+            discussion = new TaskBuilder().withName("group discussion").build();
+
+            //Manually added
+            report = new TaskBuilder().withName("write report").build();
+            powerpoint = new TaskBuilder().withName("create powerpoint for project").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
@@ -37,19 +35,19 @@ public class TypicalTestTasks {
 
         try {
             ab.addTask(new Task(assignment));
-//            ab.addTask(new Task(benson));
-//            ab.addTask(new Task(carl));
-//            ab.addTask(new Task(daniel));
-//            ab.addTask(new Task(elle));
-//            ab.addTask(new Task(fiona));
-//            ab.addTask(new Task(george));
+            ab.addTask(new Task(meeting));
+            ab.addTask(new Task(test));
+            ab.addTask(new Task(exam));
+            ab.addTask(new Task(project));
+            ab.addTask(new Task(movie));
+            ab.addTask(new Task(discussion));
         } catch (DuplicateTaskException e) {
             assert false : "not possible";
         }
     }
 
     public TestTask[] getTypicalTasks() {
-        return new TestTask[]{assignment, benson, carl, daniel, elle, fiona, george};
+        return new TestTask[]{assignment, meeting, test, exam, project, movie, discussion};
     }
 
     public TaskBook getTypicalTaskBook(){

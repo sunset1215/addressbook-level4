187d0dc4935726d0f984e25f9c0b614cce6178a4
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-10-30 22:13:01 +0800

    added Natty library and updated parser

--- src/main/java/seedu/task/logic/parser/AddParser.java
+++ src/main/java/seedu/task/logic/parser/AddParser.java
@@ -5,9 +5,15 @@ import static seedu.task.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.joestelmach.natty.DateGroup;
 
 import seedu.task.commons.exceptions.IllegalValueException;
 import seedu.task.commons.util.DateUtil;
@@ -19,9 +25,12 @@ import seedu.task.logic.commands.IncorrectCommand;
  * Parser class used to parse a add command
  */
 public class AddParser extends Parser {
+	private final Pattern NAME_FORMAT = Pattern.compile("^\\s*((\"(?<longName>.*)\")|(?<name>\\S+))\\s*.*");
+	private final Pattern FORMAT = Pattern.compile("^\\s*(?<name>\\S+)\\s*.*");
 	private final Pattern FLOATING_ARGS_FORMAT = Pattern.compile("\\s*(?<name>.+)\\s*");
 	private final Pattern DEADLINE_ARGS_FORMAT = Pattern.compile("\\s*(?<name>.+)\\s*(?<endDate>\\d{2}-\\d{2}-\\d{4})\\s*(?<endTime>\\d{2}:\\d{2})?\\s*");
 	private final Pattern EVENT_ARGS_FORMAT = Pattern.compile("\\s*(?<name>.+)\\s+(?<startDate>\\d{2}-\\d{2}-\\d{4})\\s*(?<startTime>\\d{2}:\\d{2})?\\s+(?<endDate>\\d{2}-\\d{2}-\\d{4})\\s*(?<endTime>\\d{2}:\\d{2})?\\s*");
+	private final com.joestelmach.natty.Parser parser = new com.joestelmach.natty.Parser();
 	
 	@Override
 	public Command parseCommand(String args) {
@@ -29,27 +38,25 @@ public class AddParser extends Parser {
 		boolean hasException = false;
 		
 		try {
-			if (isEventCommand(args)) {
-				toReturn = createEventTask(args);
-			} else if (isDeadlineCommand(args)) {
-				toReturn = createDeadlineTask(args);
-			} else if (isFloatingCommand(args)) {
-				toReturn = createFloatingTask(args);
+			String name = getName(args);
+			String dateString = args.replace(name, "");
+			List<LocalDateTime> dates = getDates(dateString);
+			
+			if (isEventCommand(dates)) {
+				toReturn = createEventTask(name, dates);
+			} else if (isDeadlineCommand(dates)) {
+				toReturn = createDeadlineTask(name, dates);
+			} else if (isFloatingCommand(name, dates)) {
+				toReturn = createFloatingTask(name);
 			} else {
 				throw new IllegalArgumentException();
 			}
 		} catch (NullPointerException e) {
         	hasException = true;
-        } catch (ParseException e) {
-        	hasException = true;
-        } catch (IndexOutOfBoundsException e) {
-        	hasException = true;
         } catch (IllegalArgumentException e) {
         	hasException = true;
         } catch (IllegalValueException e) {
         	hasException = true;
-        } catch (DateTimeParseException e) {
-            hasException = true;
         }
 		
 		if (hasException) {
@@ -61,100 +68,109 @@ public class AddParser extends Parser {
 		return toReturn;
 	}
 	
-	private boolean isDeadlineCommand(String args) throws NullPointerException {
-		final Matcher matcher = DEADLINE_ARGS_FORMAT.matcher(args);
-		return matcher.matches();
-	}
-	
-	private boolean isEventCommand(String args) throws NullPointerException {
-		final Matcher matcher = EVENT_ARGS_FORMAT.matcher(args);
-		return matcher.matches();
-	}
-	
-	private boolean isFloatingCommand(String args) throws NullPointerException {
-		final Matcher matcher = FLOATING_ARGS_FORMAT.matcher(args);
-		return matcher.matches();
-	}
-	
 	/**
-	 * Creates a EditCommand for a DeadlineTask given a list of arguments expects args to have the form
-	 * "[NAME] 00-00-0000"
-	 * 
-	 * @throws ParseException 
-	 * @throws IllegalArgumentException
-	 * @throws IllegalValueException 
+	 * Retrieves task name from string args
 	 */
-	private Command createDeadlineTask(String args) throws IllegalArgumentException, ParseException, IllegalValueException, DateTimeParseException {
-		Matcher matcher = DEADLINE_ARGS_FORMAT.matcher(args);
-		
+	private String getName(String args) throws IllegalArgumentException {
+		final Matcher matcher = NAME_FORMAT.matcher(args);
 		if (!matcher.matches()) {
 			throw new IllegalArgumentException();
 		}
 		
 		String name = matcher.group("name");
-		String endDateString = matcher.group("endDate");
-		String endTimeString = matcher.group("endTime");
+		String longName = matcher.group("longName");
 		
-		LocalDateTime endDate = (endTimeString == null) ? 
-			DateUtil.parseStringToLocalDate(endDateString) :
-			DateUtil.parseStringToLocalDateTime(endDateString + " " + endTimeString);
+		if (name == null && longName == null) {
+			throw new IllegalArgumentException();
+		}
 		
-        return new AddCommand(name, endDate);
+		return (name == null) ? longName : name;
 	}
 	
 	/**
-	 * Creates an EditCommand for an EventTask given a string argument that has the form
-	 * "[NAME] 00-00-0000 [00:00] 00-00-0000" [00:00]
+	 * Retrieves task dates from string args
+	 */
+	private List<LocalDateTime> getDates(String args) {
+		List<DateGroup> dateGroups = parser.parse(args);
+		if (dateGroups.size() == 0) {
+			return new ArrayList<LocalDateTime>();
+		}
+		
+		DateGroup group = dateGroups.get(0);
+		return extractLocalDates(group);
+	}
+	
+	
+	/**
+	 * Extracts the local dates from a given dateGroup
+	 */
+	private List<LocalDateTime> extractLocalDates(DateGroup dateGroup) {
+		List<Date> dates = dateGroup.getDates();
+		
+		List<LocalDateTime> localDates = new ArrayList<>();
+		for (Date date : dates) {
+			LocalDateTime local = LocalDateTime
+					.ofInstant(date.toInstant(), ZoneId.systemDefault());
+			localDates.add(local);
+		}
+		return localDates;
+	}
+	
+	private boolean isDeadlineCommand(List<LocalDateTime> dates) throws NullPointerException {
+		return dates.size() == 1;
+	}
+	
+	private boolean isEventCommand(List<LocalDateTime> dates) throws NullPointerException {
+		return dates.size() == 2;
+	}
+	
+	private boolean isFloatingCommand(String name, List<LocalDateTime> dates) throws NullPointerException {
+		return dates.size() == 0 && name != null;
+	}
+	
+	/**
+	 * Creates a EditCommand for a DeadlineTask given a name and a list containing a single date
 	 * 
-	 * @throws ParseException
+	 * @throws IllegalValueException 
 	 * @throws IllegalArgumentException 
-	 * @throws IllegalValueException 
 	 */
-	private Command createEventTask(String args) throws ParseException, IllegalArgumentException, IllegalValueException, DateTimeParseException {
-		Matcher matcher = EVENT_ARGS_FORMAT.matcher(args);
+	private Command createDeadlineTask(String name, List<LocalDateTime> dates) throws IllegalArgumentException, IllegalValueException {
+        if (dates.size() != 1 || name == null) {
+        	throw new IllegalArgumentException();
+        }
+        
+        LocalDateTime endDate = dates.get(0);
 		
-		if (!matcher.matches()) {
-			throw new IllegalArgumentException();
-		}
+		return new AddCommand(name, endDate);
+	}
+	
+	/**
+	 * Creates an EditCommand for an EventTask given a name and a list of dates
+	 * 
+	 * @throws IllegalValueException 
+	 * @throws IllegalArgumentException 
+	 */
+	private Command createEventTask(String name, List<LocalDateTime> dates) throws IllegalArgumentException, IllegalValueException {
+		if (dates.size() != 2 || name == null) {
+        	throw new IllegalArgumentException();
+        }
 		
-		String name = matcher.group("name");
-		String startDateString = matcher.group("startDate");
-		String endDateString = matcher.group("endDate");
+		LocalDateTime startDate = dates.get(0);
+		LocalDateTime endDate = dates.get(1);
 		
-		String startTimeString = matcher.group("startTime");
-		String endTimeString = matcher.group("endTime");
-		
-		LocalDateTime startDate = (startTimeString == null) ? 
-				DateUtil.parseStringToLocalDate(startDateString) :
-				DateUtil.parseStringToLocalDateTime(startDateString + " " + startTimeString);
-		
-		LocalDateTime endDate = (endTimeString == null) ? 
-				DateUtil.parseStringToLocalDate(endDateString) :
-				DateUtil.parseStringToLocalDateTime(endDateString + " " + endTimeString);
-		
-		if (startDate.isAfter(endDate)) {
-			throw new IllegalArgumentException();
-		}
-				
         return new AddCommand(name, startDate, endDate);
 	}
 	
 	/**
-	 * Creates an AddCommand for a Task given an index and a name
-	 * "[INDEX] thisisanewname"
+	 * Creates an AddCommand for a Task given a name
 	 * 
-	 * @throws ParseException 
-	 * @throws IllegalArgumentException
-	 * @throws IllegalValueException 
+	 * @throws IllegalValueException
+	 * @throws IllegalArgumentException 
 	 */
-	private Command createFloatingTask(String args) throws IllegalArgumentException, ParseException, IllegalValueException {
-		Matcher matcher = FLOATING_ARGS_FORMAT.matcher(args);
-		
-		if (!matcher.matches()) {
-			throw new IllegalArgumentException();
-		}
-		
-		String name = matcher.group("name").trim();
+	private Command createFloatingTask(String name) throws IllegalArgumentException, IllegalValueException {
+		if (name == null) {
+        	throw new IllegalArgumentException();
+        }
 		
         return new AddCommand(name);
 	}

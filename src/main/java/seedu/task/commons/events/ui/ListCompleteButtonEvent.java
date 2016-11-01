9801e0cd15334dd17022c8f166f3876840ff223a
Torasian <williamhawken@hotmail.co.uk>
2016-11-01 16:02:31 +0800

    added listCompleteButtonClass

---
+++ src/main/java/seedu/task/commons/events/ui/ListCompleteButtonEvent.java
@@ -0,0 +1,19 @@
+//@@author A0153723J
+package seedu.task.commons.events.ui;
+
+import seedu.task.commons.events.BaseEvent;
+import seedu.task.model.task.Status;
+
+public class ListCompleteButtonEvent extends BaseEvent{
+
+
+	public ListCompleteButtonEvent(boolean status) {
+		status = Status.STATUS_COMPLETE;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName();
+	}
+	
+}

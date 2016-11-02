61eb5a6256b412991969d39ab47b9d51f070f34b
VivianLow <vivian198912@gmail.com>
2016-11-02 22:57:36 +0800

    Fix StoreCommandTest to have no errors only

--- src/test/java/guitests/StoreCommandTest.java
+++ src/test/java/guitests/StoreCommandTest.java
@@ -5,28 +5,24 @@ import org.junit.Test;
 import seedu.task.logic.commands.StoreCommand;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.task.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.task.logic.commands.StoreCommand.MESSAGE_SUCCESS;
 
 public class StoreCommandTest extends TaskBookGuiTest{
 	
 	@Test
-	public void Store(){
-		
-		
-		//check command for correct directory
-		commandBox.runCommand("store");
-		assertStoreSuccess("store + c:\\Users ", StoreCommand.MESSAGE_SUCCESS);
-		
-		//check command for an incorrect directory
+	public void store_invalidPath() {
 		commandBox.runCommand("store + 123");
-		assertResultMessage("Invlaid Directory Path");
-		
-		
+		assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, StoreCommand.MESSAGE_USAGE));
+	}
+	
+	@Test
+	public void store_validPath() {
+	    
 	}
 
 	private void assertStoreSuccess(String command, String expectedMessage) {
 		commandBox.runCommand(command);
 		assertResultMessage(expectedMessage);
-		
 	}
 }
